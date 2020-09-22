a4b4db588d5c2cc22f3194078b459a384322884d
airmeno <71429025+airmeno@users.noreply.github.com>
2020-09-22 15:24:05 +0300

    Delete zabbix_telegram_alert.sh

--- zabbix_telegram_alert.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-token=''
-
-chat="$1"
-subj="$2"
-message="$3"
-
-/usr/bin/curl -s --header 'Content-Type: application/json' --request 'POST' --data "{\"chat_id\":\"${chat}\",\"text\":\"${subj}\n${message}\"}" "https://api.telegram.org/bot${token}/sendMessage"
